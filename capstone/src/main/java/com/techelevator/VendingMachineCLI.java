063ac4fd69204a7c40e5621342db6d688e7c0dc8
Kimberly Bryant <bryantkim702@gmail.com>
2022-04-04 10:03:11 -0700

    purchase menu populated

--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -2,8 +2,11 @@ package com.techelevator;
 
 import com.techelevator.view.Inventory;
 import com.techelevator.view.Menu;
+import com.techelevator.view.PurchaseMenu;
 
 import java.io.InputStream;
+
+
 
 public class VendingMachineCLI {
 
@@ -12,21 +15,29 @@ public class VendingMachineCLI {
 	private static final String MAIN_MENU_OPTION_EXIT = "Exit";
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT };
 
+	private static final String PURCHASE_MENU_OPTION_FEED_MONEY = "Feed Money";
+	private static final String PURCHASE_MENU_OPTION_SELECT_PRODUCT = "Select Product";
+	private static final String PURCHASE_MENU_OPTION_FINISH_TRANSACTION = "Finish Transaction";
+	private static final String[] PURCHASE_MENU_OPTIONS = {PURCHASE_MENU_OPTION_FEED_MONEY, PURCHASE_MENU_OPTION_SELECT_PRODUCT, PURCHASE_MENU_OPTION_FINISH_TRANSACTION};
+
 	private Menu menu;
-	private Inventory inventory = new Inventory(System.in, System.out);
+	private Inventory inventory = new Inventory();
+	private PurchaseMenu purchase = new PurchaseMenu(System.in, System.out);
 
 	public VendingMachineCLI(Menu menu) {
 		this.menu = menu;
 	}
 
 	public void run() {
+		inventory.populateInventory();
 		while (true) {
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
-				inventory.populateInventory();
+				inventory.displayInventory();
 				// display vending machine items
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
+				String itemPurchase = (String) purchase.getChoiceFromOptions(PURCHASE_MENU_OPTIONS);
 				// do purchase
 			} else if (choice.equals(MAIN_MENU_OPTION_EXIT)) {
 				System.out.println("Thank you for your purchase");
@@ -38,6 +49,7 @@ public class VendingMachineCLI {
 	public static void main(String[] args) {
 		Menu menu = new Menu(System.in, System.out);
 		VendingMachineCLI cli = new VendingMachineCLI(menu);
+
 		cli.run();
 	}
 }
--- capstone/src/main/java/com/techelevator/view/Inventory.java
+++ capstone/src/main/java/com/techelevator/view/Inventory.java
@@ -1,30 +1,35 @@
 package com.techelevator.view;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class Inventory extends Menu{
-    private File inventoryList = new File("C:\\Users\\First\\Desktop\\Kimberly Bryant Student Exercises\\Pair Programming\\module-1-capstone\\capstone\\vendingmachine.csv");
+public class Inventory{
 
-    public Inventory(InputStream input, OutputStream output) {
-        super(input, output);
-    }
+    private List<String> vendingItems = new ArrayList<>();
+    private File inventoryList = new File("C:\\Users\\First\\Desktop\\Kimberly Bryant Student Exercises\\Pair Programming\\module-1-capstone\\capstone\\vendingmachine.csv");
 
 
     public void populateInventory() {
     try (Scanner inventoryScanner = new Scanner(inventoryList)){
         while (inventoryScanner.hasNextLine()) {
-            System.out.println(inventoryScanner.nextLine());
+
+            vendingItems.add(inventoryScanner.nextLine());
         }
     }catch (FileNotFoundException e){
         System.out.println(e.getMessage());
     }
     }
-
-
+    public void displayInventory(){
+        try (Scanner inventoryScanner = new Scanner(inventoryList)){
+            while (inventoryScanner.hasNextLine()) {
+                System.out.println(inventoryScanner.nextLine());
+            }
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+    }
 
 
 
