22e37a9bb99b5bebb9a9bf0373704358f84bc2f6
Kimberly Bryant <bryantkim702@gmail.com>
2022-04-08 12:40:55 -0700

    Simplfied classes

--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,9 +1,9 @@
 package com.techelevator;
 
-import com.techelevator.view.Inventory;
-import com.techelevator.view.Menu;
 
-import java.util.Scanner;
+import com.techelevator.view.Menu;
+import com.techelevator.view.VendingMachine;
+
 
 
 public class VendingMachineCLI {
@@ -18,21 +18,23 @@ public class VendingMachineCLI {
 	private static final String PURCHASE_MENU_OPTION_SELECT_PRODUCT = "Select Product";
 	private static final String PURCHASE_MENU_OPTION_FINISH_TRANSACTION = "Finish Transaction";
 	private static final String[] PURCHASE_MENU_OPTIONS = {PURCHASE_MENU_OPTION_FEED_MONEY, PURCHASE_MENU_OPTION_SELECT_PRODUCT, PURCHASE_MENU_OPTION_FINISH_TRANSACTION};
+	private static final String FILE_PATH = "C:\\Users\\First\\Desktop\\Kimberly Bryant Student Exercises\\Pair Programming\\module-1-capstone\\capstone\\vendingmachine.csv";
 
 	private Menu menu;
-	private Inventory inventory = new Inventory();
+	private VendingMachine vendingMachine;
 
-	public VendingMachineCLI(Menu menu) {
+	public VendingMachineCLI(Menu menu, VendingMachine vendingMachine) {
 		this.menu = menu;
+		this.vendingMachine = vendingMachine;
 	}
 
 	public void run() {
-		inventory.populateInventory();
+		vendingMachine.populateInventory();
 		while (true) {
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
-				inventory.displayInventory();
+				vendingMachine.displayInventory();
 				// display vending machine items
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				double balance = 0.0;
@@ -41,7 +43,7 @@ public class VendingMachineCLI {
 
 
 				} else if (itemPurchase.equals(PURCHASE_MENU_OPTION_SELECT_PRODUCT)) {
-					inventory.displayInventory();
+					vendingMachine.displayInventory();
 					String slot = menu.getStringFromUser("Enter the slot to vend: ").toUpperCase();
 					//if inventory list contains product code provided, then it will decrease stock by 1,
 					// update balance to remove price of item. then print item name cost,money remaining and item message.
@@ -59,7 +61,8 @@ public class VendingMachineCLI {
 
 			public static void main (String[]args){
 				Menu menu = new Menu(System.in, System.out);
-				VendingMachineCLI cli = new VendingMachineCLI(menu);
+				VendingMachine vendingMachine = new VendingMachine(FILE_PATH);
+				VendingMachineCLI cli = new VendingMachineCLI(menu, vendingMachine);
 
 				cli.run();
 			}
--- capstone/src/main/java/com/techelevator/view/VendingMachine.java
+++ capstone/src/main/java/com/techelevator/view/VendingMachine.java
@@ -1,9 +1,50 @@
 package com.techelevator.view;
 
-public class VendingMachine {
-    private static Inventory inventory;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-    public VendingMachine (Inventory inventory){
-        this.inventory = inventory;
+public class VendingMachine {
+    private Map<String, Product> items = new LinkedHashMap<>();
+    private File inventoryList = new File("C:\\Users\\First\\Desktop\\Kimberly Bryant Student Exercises\\Pair Programming\\module-1-capstone\\capstone\\vendingmachine.csv");
+
+    public VendingMachine (String filePath){
+        inventoryList = new File(filePath);
+        populateInventory();
+    }
+
+    public void populateInventory() {
+        try (Scanner inventoryScanner = new Scanner(inventoryList)){
+            while (inventoryScanner.hasNextLine()) {
+                String [] itemArray = inventoryScanner.nextLine().split("\\|");
+                items.put(itemArray [0],new Product(itemArray));
+
+            }
+
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    public void displayInventory(){
+        System.out.println();
+        System.out.println("VENDING MACHINE ITEMS");
+        System.out.println();
+        System.out.printf("%-5s %-8s %-20s %-8s\n", "Slot", "Price", "Name", "Left");
+        System.out.println("-----------------------------------------");
+        for (Map.Entry<String, Product> entry: items.entrySet())
+        {
+            Product item = entry.getValue();
+            if (item.getStock() < 1) {
+//                Vendable soldOutItem = soldOutItems.get(slot);
+                System.out.printf("%-5s %-8s %-20s %-15s\n", item.getSlot(), item.getStrPrice(), item.getName(), "SOLD OUT!");
+                continue;
+            }else{
+
+                System.out.printf("%-5s %-8s %-20s %-15s\n", item.getSlot(), item.getStrPrice(), item.getName(), item.getStock());
+            }
+
+        }
     }
 }
