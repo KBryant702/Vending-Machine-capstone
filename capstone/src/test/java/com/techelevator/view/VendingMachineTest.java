ef5d822382bd92f3b48751d7939007c8229a4e2d
Kimberly Bryant <bryantkim702@gmail.com>
2022-04-14 13:04:30 -0700

    Inventory list file correctly reading to test

--- capstone/src/test/java/com/techelevator/view/VendingMachineTest.java
+++ capstone/src/test/java/com/techelevator/view/VendingMachineTest.java
@@ -7,39 +7,39 @@ import org.junit.Test;
 
 public class VendingMachineTest {
     VendingMachine vendingMachine;
-@Before
-public void getVendingMachineInstance(){
-    this.vendingMachine = new VendingMachine("capstone/vendingmachine.csv");
-}
-@Test
+
+    @Before
+    public void getVendingMachineInstance() {
+        this.vendingMachine = new VendingMachine("capstone/vendingmachine.csv");
+    }
+
+    @Test
     public void zero_balance_returns_zero_dollars() {
-    Assert.assertEquals("$0.00", vendingMachine.getStrBalance());
-}
+        Assert.assertEquals("$0.00", vendingMachine.getStrBalance());
+    }
 
-@Test
-    public void positive_balance_returns_positive_dollars(){
-    vendingMachine.feedMoney(5);
-    Assert.assertEquals("$5.00", vendingMachine.getStrBalance());
-}
+    @Test
+    public void positive_balance_returns_positive_dollars() {
+        vendingMachine.feedMoney(5);
+        Assert.assertEquals("$5.00", vendingMachine.getStrBalance());
+    }
 
-@Test
-public void positive_balance_returns_change(){
-    vendingMachine.feedMoney(5);
-    Assert.assertEquals("Returned Change: 20 Quarter(s), 0 Dime(s) & 0 Nickel(s)!", vendingMachine.returnChange());
-}
+    @Test
+    public void positive_balance_returns_change() {
+        vendingMachine.feedMoney(5);
+        vendingMachine.vend("D1");
+        Assert.assertEquals("Returned Change: 16 Quarter(s), 1 Dime(s) & 1 Nickel(s)!", vendingMachine.returnChange());
+    }
 
-@Test
+    @Test
     public void zero_balance_returns_empty_string() {
-    Assert.assertEquals("", vendingMachine.returnChange());
-}
+        Assert.assertEquals("", vendingMachine.returnChange());
+    }
 
-//@Test
-//    public void calculate_change(){}
-//@Test
-//     public void feed_money() {}
-//@Test
-//public void vend() {}
-//@Test
-//public void populate_inventory() {}
+//    @Test Make sure available items are vended
+    // @Test sold out
+    // @Test not enough money
+    //@Test Incorrect Slot
+
 
 }
